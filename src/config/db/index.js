1eb556be8667581c71983a3f0d7e402942903b14
tvkspor <111938970+tvkspor@users.noreply.github.com>
2024-03-07 22:41:37 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb+srv://tmtuan120704:tvk553266@cluster0.rxoml7p.mongodb.net/f8_education_dev");
+    await mongoose.connect("mongodb://localhost:27017/f8_education_dev");
     console.log("success");
   } catch (error) {
     console.log("fail");
